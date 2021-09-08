c6ddba4f3ee822179d882c5c5db97895cd98dafe
Mario Juez-Gil <mariojg@ubu.es>
2021-09-08 12:12:04 +0200

    implemented stratified repeated cross validation

--- src/main/scala/org/ubu/admirable/exec/ExperimentLauncher.scala
+++ src/main/scala/org/ubu/admirable/exec/ExperimentLauncher.scala
@@ -17,6 +17,42 @@
 
 package org.ubu.admirable.exec
 
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
 object ExperimentLauncher {
+
+  def splitDataset(ds: Dataset[Row], folds: Int, seed: Long): Array[Dataset[Row]] = {
+    val weight = 1.0 / folds.toDouble
+    ds.randomSplit((1 to folds).map{_ => weight}.toArray, seed)
+  }
+
+  def makeFolds(ds: Dataset[Row], reps: Int, folds: Int, seed: Long): Array[Array[Dataset[Row]]] = {
+    val labels = ds.groupBy("label")
+      .count
+      .collect
+      .map{r => r.getAs[Double](0)}
+
+    val dsSplits = labels.map{l => ds.filter(col("label") === l)} // one dataset per label
     
+    // data set repetitions and stratified folds
+    (1 to reps).map{ rep =>
+      dsSplits.map{ split =>
+        splitDataset(split, folds, seed)
+      }.reduce{ (res, part) =>
+        (1 to res.length).map{ i =>
+          res(i).union(part(i))
+        }.toArray
+      }
+    }.toArray
+  }
+
+  def main(args: Array[String]) {
+    val session = SparkSession.builder.appName("LSH-IS-Spark experiment launcher")
+      .getOrCreate()
+    session.sparkContext.setLogLevel("WARN")
+  }
+
 }
