ea368d438d6a8996c985ffcdf4e615f235e8d880
Mario Juez-Gil <mariojg@ubu.es>
2021-09-09 09:24:42 +0200

    working implementation

--- src/main/scala/org/ubu/admirable/exec/ExperimentLauncher.scala
+++ src/main/scala/org/ubu/admirable/exec/ExperimentLauncher.scala
@@ -21,6 +21,12 @@ import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import scala.util.Random
+import org.ubu.admirable.util.LibSVMReader
+import org.apache.spark.ml.instance.LSHIS
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import java.time.Instant
+import java.time.Duration
 
 object ExperimentLauncher {
 
@@ -42,17 +48,118 @@ object ExperimentLauncher {
       dsSplits.map{ split =>
         splitDataset(split, folds, seed)
       }.reduce{ (res, part) =>
-        (1 to res.length).map{ i =>
+        (0 to res.length - 1).map{ i =>
           res(i).union(part(i))
         }.toArray
       }
     }.toArray
   }
 
+  def trainTestSplits(repsAndFolds: Array[Array[Dataset[Row]]]): Array[Array[(Dataset[Row], Dataset[Row])]] = {
+    repsAndFolds.map { folds =>
+      val ids = (0 until folds.length).toArray
+      ids.map { testId =>
+        val idsBuffer = ids.toBuffer
+        idsBuffer.remove(testId)
+        val trainFold = idsBuffer.map{ trainId =>
+          folds(trainId)
+        }.reduce{ (res, part) =>
+          res.union(part)
+        }
+        val testFold = folds(testId)
+        (trainFold, testFold)
+      }
+    }
+  }
+
   def main(args: Array[String]) {
     val session = SparkSession.builder.appName("LSH-IS-Spark experiment launcher")
       .getOrCreate()
     session.sparkContext.setLogLevel("WARN")
+
+    val rnd = new Random(46)
+
+    val inputDataset = args(0)
+    val bucketLength = args(1).toInt
+    val numHashTablesAnd = args(2).toInt
+    val numHashTablesOr = args(3).toInt
+    val cvReps = args(4).toInt
+    val cvFolds = args(5).toInt
+    val outputPath = args(6)
+
+    val dataset = LibSVMReader.libSVMToML(inputDataset, session)
+      .select(col("label"), col("features")).cache()
+
+    val repsFolds = makeFolds(dataset, cvReps, cvFolds, rnd.nextLong)
+    val repsTrainTestFolds = trainTestSplits(repsFolds)
+    val (resBase, resLSH) = repsTrainTestFolds.zipWithIndex.map{ case (folds, nrep) =>
+      folds.zipWithIndex.map { case ((trainFold, testFold), nfold) =>
+        
+        val cachedTrainFold = trainFold.cache
+        cachedTrainFold.count // force cache
+        val cachedTestFold = testFold.cache
+        cachedTestFold.count // force cache
+        
+        val lshis = new LSHIS()
+          .setBucketLength(bucketLength)
+          .setNumHashTablesAnd(numHashTablesAnd)
+          .setNumHashTablesOr(numHashTablesOr)
+          .setSeed(rnd.nextLong)
+        
+        val init = Instant.now
+        val filteredTrainFold = lshis.transform(cachedTrainFold).cache
+        filteredTrainFold.count
+        val stop = Instant.now
+        val milis = Duration.between(init, stop).toMillis
+        
+        filteredTrainFold.count // force cache
+
+        // training models
+        val dt = new DecisionTreeClassifier().setSeed(rnd.nextLong)
+        val baseDTModel = dt.fit(cachedTrainFold)
+        val lshDTModel = dt.fit(filteredTrainFold)
+
+        // predictions
+        val baseResults = baseDTModel
+          .transform(cachedTestFold)
+          .select(col("label"), col("prediction"))
+          .withColumnRenamed("label", "true")
+          .withColumnRenamed("prediction", "predicted")
+          .withColumn("fold", lit(nfold))
+          .withColumn("repetition", lit(nrep)).cache
+        val lshResults = lshDTModel
+          .transform(cachedTestFold)
+          .select(col("label"), col("prediction"))
+          .withColumnRenamed("label", "true")
+          .withColumnRenamed("prediction", "predicted")
+          .withColumn("fold", lit(nfold))
+          .withColumn("repetition", lit(nrep))
+          .withColumn("time", lit(milis)).cache
+
+        cachedTrainFold.unpersist(true)
+        filteredTrainFold.unpersist(true)
+        cachedTestFold.unpersist(true)
+
+        (baseResults, lshResults)
+      }.reduce{ (res, part) =>
+        val resBase = res._1
+        val resLSH = res._2
+        val partBase = part._1
+        val partLSH = part._2
+
+        (resBase.union(partBase), resLSH.union(partLSH))
+      }
+    }.reduce{ (res, part) =>
+      val resBase = res._1
+      val resLSH = res._2
+      val partBase = part._1
+      val partLSH = part._2
+
+      (resBase.union(partBase), resLSH.union(partLSH))
+    }
+
+    resBase.write.csv(outputPath + "/base")
+    resLSH.write.csv(outputPath + "/lsh")
   }
 
 }
